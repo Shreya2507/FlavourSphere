83da6f1c63df96048c0cd4a82814a99ee352e732
Khyati Dhawan <khyati.dhwn22@gmail.com>
2024-07-27 12:57:42 +0530

    changes done

---
+++ src/components/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+      <div className="section-container bg-custom-gradient min-h-screen">
+          
+
+     </div>
+  );
+}
+
+export default Banner
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "custom-gradient":
+          "linear-gradient(to bottom, #FFCCD5 0%, #FED9E0 27%, #FED9E0 40%, #FED9E0 85%, #FCFCFC 100%)",
+      },
+    },
   },
-  plugins: [],
-}
-
+  plugins: [require("daisyui")],
+};
