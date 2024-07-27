7a2755e73f48c80e82684ca9f82e365062457b34
Shreya Mathur <smathur245@gmail.com>
2024-07-27 12:55:23 +0530

    added navbar and basic router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from './components/Navbar'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      NEW helloooooooooo
-      heelo1
-        
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        {/* <Route path='/' element={<Home />} /> */}
+      </Routes>
+    </BrowserRouter>
   )
 }
 
