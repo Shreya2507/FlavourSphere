b1661a4a72cae354da426c4878275e9f8a6ea814
Khyati Dhawan <khyati.dhwn22@gmail.com>
2024-08-02 21:07:24 +0530

    Donor

--- src/pages/home/Doner.jsx
+++ src/pages/home/Doner.jsx
@@ -4,7 +4,7 @@ const Doner = () => {
     return (
       <div className="h-60 bg-doner-gradient max-w-screen-2xl container mx-auto xl:px-24 px-4 pt-20 mb-20 mt-10 ">
         <div className="  flex-col items-center justify-between ">
-          <h2 className="title text-center mx-auto  ">
+          <h2 className="title text-center mx-auto text">
             Find What You’re Seeeking{" "}
           </h2>
           <p className="my-5 text-[#6A6868] leading-[32px] font-medium mx-auto text-center">
@@ -13,10 +13,10 @@ const Doner = () => {
           </p>
           <div className="flex flex-row items-center justify-center">
             <button className="mx-10 btn hover:bg-[#ba4545] hover:border-none rounded-full border-none bg-[#D95B5B] text-white px-8 py-3 rounded-full border-none">
-              Explore
+              Acceptor
             </button>
             <button className="btn hover:bg-[#ba4545] hover:border-none rounded-full border-none bg-[#D95B5B] text-white px-8 py-3 rounded-full border-none">
-              Explore
+              Donor
             </button>
           </div>
         </div>
