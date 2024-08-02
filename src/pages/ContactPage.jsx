fbdc508c3f548405743a6ab6566dbb56897501c3
Khyati Dhawan <khyati.dhwn22@gmail.com>
2024-08-02 19:51:00 +0530

    blur Contact Page

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -27,8 +27,10 @@ const ContactUsPage = () => {
 
   return (
     <div className=" max-w-screen-2xl container mx-auto xl:px-20 px-4 py-10 mt-20 bg-custom-gradient min-h-screen">
-      <div className="w-1/2 mx-auto p-6 bg-white shadow-md rounded-lg justify-center items-center ">
-        <h1 className="text-3xl font-bold mb-6 text-center text-gray-500">Contact Us</h1>
+      <div className="bg-opacity-30 bg-yellow glass w-1/2 mx-auto p-6 shadow-md rounded-lg justify-center items-center ">
+        <h1 className="text-3xl font-bold mb-6 text-center text-gray-500">
+          Contact Us
+        </h1>
         <form onSubmit={handleSubmit} className="flex flex-col space-y-4">
           <div className="form-control">
             <label htmlFor="name" className="label">
@@ -43,7 +45,7 @@ const ContactUsPage = () => {
               value={formData.name}
               onChange={handleChange}
               placeholder="Enter Your Name"
-              className="input input-bordered w-full bg-gray-100"
+              className=" placeholder-gray-500 bg-opacity-50 input input-bordered w-full bg-gray-100"
               required
             />
           </div>
@@ -60,7 +62,7 @@ const ContactUsPage = () => {
               value={formData.email}
               placeholder="Enter Your Email"
               onChange={handleChange}
-              className="input input-bordered w-full bg-gray-100"
+              className=" placeholder-gray-500 bg-opacity-50 input input-bordered w-full bg-gray-100"
               required
             />
           </div>
@@ -76,7 +78,7 @@ const ContactUsPage = () => {
               value={formData.message}
               onChange={handleChange}
               placeholder="Leave a Message here . . ."
-              className="textarea textarea-bordered w-full h-32 bg-gray-100"
+              className="textarea textarea-bordered w-full h-32 bg-white placeholder-gray-500 bg-opacity-40"
               required
             ></textarea>
           </div>
