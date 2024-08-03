cded74c7139aa597c419a3e46d9c9e0b1ca31e5a
Khyati Dhawan <khyati.dhwn22@gmail.com>
2024-08-03 15:19:17 +0530

    buttons adjusted

--- src/pages/home/Doner.jsx
+++ src/pages/home/Doner.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Doner = () => {
     return (
-      <div className="h-60 bg-doner-gradient max-w-screen-2xl container mx-auto xl:px-24 px-4 pt-20 mb-20 mt-10 ">
+      <div className=" bg-doner-gradient max-w-screen-2xl container mx-auto xl:px-24 px-4 pt-20 mb-20 mt-10 ">
         <div className="  flex-col items-center justify-between ">
           <h2 className="title text-center mx-auto text">
             Find What You’re Seeeking{" "}
@@ -12,8 +12,8 @@ const Doner = () => {
             Our platform is designed to connect passionate cooks with those in
             need, creating a bridge between recipe enthusiasts and food donors.
           </p>
-          <div className="flex flex-row items-center justify-center">
-            <Link to="/Acceptorportal">
+          <div className="flex flex-row items-center justify-center ">
+            <Link to="/Aportal">
               <button className="mx-10 btn hover:bg-[#ba4545] hover:border-none rounded-full border-none bg-[#D95B5B] text-white px-8 py-3 rounded-full border-none">
                 Acceptor
               </button>
